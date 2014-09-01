4bd4b8edff116d607496cdcc06a418635b92d456
Ovi Sofariu <ovi@sofariu.com>
2014-09-01 11:11:20 -0400

    efficient and cleaner

--- src/main/groovy/life/World.groovy
+++ src/main/groovy/life/World.groovy
@@ -1,32 +1,45 @@
 package life
 
 class World {
-    def currentCells
+    HashSet  currentCells
 
     World(currentCells = []) {
         this.currentCells = currentCells
     }
 
     World iterate() {
-        def nextGenCells = []
-        Set potentialNewCells = []
-        currentCells.each { Cell cell ->
-            potentialNewCells += getNeighboringLocations(cell)
-            def nc = neighborCount(cell)
-            if (nc in 2..3) {
-                nextGenCells.add(cell)
-            }
-        }
-        resurrectWorthyCells(potentialNewCells).each {
-            nextGenCells += it
-        }
-
-        new World(nextGenCells)
+        new World (updateCurrentCells() +  resurrectWorthyCells())
     }
 
-    Cell[] resurrectWorthyCells(potentialCells) {
-        def worthyCells = []
-        potentialCells.each { cell ->
+    def size() {
+        return currentCells.size()
+    }
+
+    Set updateCurrentCells() {
+        def nextGenCells = [] as Set
+        currentCells.each { Cell currentCell ->
+            if (neighborCount(currentCell) in 2..3) {
+                nextGenCells += currentCell
+            }
+        }
+        nextGenCells
+    }
+
+    int neighborCount(Cell cell) {
+        def count = 0
+        makeNeighborCells(cell).each {
+           count += cellIsAlive(it) ? 1 : 0
+        }
+        count
+    }
+
+    boolean cellIsAlive(cell) {
+        currentCells.contains(cell)
+    }
+
+    Set resurrectWorthyCells() {
+        Set worthyCells = []
+        generatePotentialNewCells().each {Cell cell ->
             if (neighborCount(cell) == 3 && !cellIsAlive(cell)) {
                 worthyCells.add(cell)
             }
@@ -34,33 +47,23 @@ class World {
         worthyCells
     }
 
-    boolean cellIsAlive(cell) {
-        currentCells.find { it.isAt(cell.x, cell.y) }
-    }
-
-    int neighborCount(Cell cell) {
-        def count = 0
-        getNeighboringLocations(cell).each {
-            if (cellIsAlive(it))
-                count++
+    Set generatePotentialNewCells() {
+        Set potentialNewCells = []
+        currentCells.each { Cell currentCell ->
+            potentialNewCells +=  makeNeighborCells(currentCell)
         }
-        count
+        potentialNewCells
     }
 
-    private static def getNeighboringLocations(Cell cell) {
-        def neighbors = []
+    private static Set makeNeighborCells(Cell cell) {
         def x = cell.x, y = cell.y
-
+        Set neighborCells = []
         for (nx in x - 1..x + 1) {
             for (ny in y - 1..y + 1) {
-                addNeighborCellToList(cell, nx, ny, neighbors)
+                neighborCells += new Cell(nx, ny)
             }
         }
-        neighbors
-    }
-
-    private static void addNeighborCellToList(Cell cell, int x, int y, neighbors) {
-        if (!cell.isAt(x, y))
-            neighbors.add(new Cell(x, y))
+        neighborCells.remove(cell)
+        neighborCells
     }
 }
